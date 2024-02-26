f574d098a9d0720cfcee625e63d30129e4f4dd41
Qian Sun <69973222+Chin-Sun@users.noreply.github.com>
2024-02-26 17:52:07 -0500

    Create Class TicketInspector

---
+++ Class/TicketInspector.java
@@ -0,0 +1,17 @@
+public class TicketInspector {
+    // Attributes
+    private int inspectorId;
+    private String name;
+
+    // Constructor
+    public TicketInspector(int inspectorId, String name) {
+        this.inspectorId = inspectorId;
+        this.name = name;
+    }
+
+    // Method
+    public void checkTicket(String ticketDetails) {
+        // Logic to check if the ticket is valid
+        System.out.println(name + " checks ticket: " + ticketDetails);
+    }
+}
