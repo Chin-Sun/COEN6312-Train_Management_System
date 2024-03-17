0a261839de10d392d18b21c692beace081b1c279
anilani327 <124096996+anilani327@users.noreply.github.com>
2024-03-17 14:46:15 -0400

    Update TrainOperator.java

--- Milestone3 OCL Code/TrainOperator.java
+++ Milestone3 OCL Code/TrainOperator.java
@@ -1,56 +1,12 @@
-
-
 public class TrainOperator {
+    private String operatorID;
 
-	/**
-	 * 
-	 */
-	private String name;
-	/**
-	 * 
-	 */
-	private int Id;
-	/**
-	 * Getter of name
-	 */
-	public String getName() {
-	 	 return name; 
-	}
-	/**
-	 * Setter of name
-	 */
-	public void setName(String name) { 
-		 this.name = name; 
-	}
-	/**
-	 * Getter of Id
-	 */
-	public int getId() {
-	 	 return Id; 
-	}
-	/**
-	 * Setter of Id
-	 */
-	public void setId(int Id) { 
-		 this.Id = Id; 
-	}
-	/**
-	 * 
-	 * @param trainId 
-	 * @return 
-	 */
-	public String trackTrainStatus(String trainId) { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param message 
-	 * @return 
-	 */
-	public String notifyEmergency(String message) { 
-		// TODO Auto-generated method
-		return null;
-	 } 
+    public TrainOperator(String operatorID) {
+        if (!Dispatcher.isIDUnique(operatorID)) {
+            throw new IllegalArgumentException("Operator ID must not be the same as any Dispatcher's ID.");
+        }
+        this.operatorID = operatorID;
+    }
 
+    // Getter and other methods...
 }
